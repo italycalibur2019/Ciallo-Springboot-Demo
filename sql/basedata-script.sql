ee914dff91dd9781bda657d213b1914465008d4e
dhr <dhr@cqkgkj.com>
2024-12-24 18:17:39 +0800

    perf:补充数据表数据

--- sql/basedata-script.sql
+++ sql/basedata-script.sql
@@ -25,3 +25,25 @@ COMMENT ON COLUMN basedata.td_driver.first_name IS '名字';
 COMMENT ON COLUMN basedata.td_driver.last_name IS '姓氏';
 COMMENT ON COLUMN basedata.td_driver.code IS '车手编号';
 COMMENT ON COLUMN basedata.td_driver.country IS '国家';
+
+
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(1, 'Max', 'Verstappen', 1, 'Netherlands');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(2, 'Sergio', 'Perez', 11, 'Mexico');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(3, 'Fernando', 'Alonso', 14, 'Spain');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(4, 'Charles', 'Leclerc', 16, 'Monaco');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(5, 'Carlos', 'Sainz', 55, 'Spain');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(6, 'Lance', 'Stroll', 18, 'Canada');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(7, 'Lando', 'Norris', 4, 'Great Britain');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(8, 'Oscar', 'Piastri', 81, 'Australia');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(9, 'Pierre', 'Gasly', 10, 'France');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(10, 'Esteban', 'Ocon', 31, 'France');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(11, 'Nico', 'Hulkenberg', 27, 'Germany');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(12, 'Kevin', 'Magnussen', 20, 'Denmark');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(13, 'Yuki', 'Tsunoda', 22, 'Japan');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(14, 'Daniel', 'Riccaiardo', 3, 'Australia');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(15, 'Alexander', 'Albon', 23, 'Thailand');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(16, 'Logan', 'Sargeant', 2, 'America');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(17, 'Valtteri', 'Bottas', 77, 'Finland');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(18, 'Guanyu', 'Zhou', 24, 'China');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(19, 'Lewis', 'Hamilton', 44, 'Great Britain');
+INSERT INTO basedata.td_driver (id, first_name, last_name, code, country) VALUES(20, 'George', 'Russell', 63, 'Great Britain');
